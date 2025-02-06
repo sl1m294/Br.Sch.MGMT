691e1eceb78c075f9b805168ab5ce69052a1b06d
salimo <ps.erisa@outlook.com>
2025-02-05 22:35:04 -0500

    Major Changes

---
+++ src/app/(dashboard)/layout.tsx
@@ -0,0 +1,42 @@
+import type { Metadata } from "next";
+import Link from "next/link";
+import Image from "next/image";
+import Menu from "../../components/Menu";
+import Navbar from "../../components/Navbar";
+
+import { Inter } from "next/font/google";
+// import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+  title: "Lama Dev School Management Dashboard",
+  description: "Next.js School Management System",
+};
+
+export default function DashboardLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <div className={`h-screen flex ${inter.className}`}>
+      {/*LEFT */}
+      <div className="w-[14%] md:w-[8%] lg:w-[16%] xl:w-[14%] ">
+        <Link
+          href="/"
+          className="flex items-center justify-center lg:justify-start gap-2 p-4"
+        >
+          <Image src="/blogoo.png" alt="logo" width={32} height={32} />
+          <span className="hidden lg:block">Brooks School</span>
+        </Link>
+        <Menu />
+      </div>
+      {/*RIGHT */}
+      <div className="w-[86%] md:w-[92%] lg:w-[84%] xl:w-[86%] bg-[#F7F8FA] overflow-scroll">
+        <Navbar />
+        {children}
+      </div>
+    </div>
+  );
+}
